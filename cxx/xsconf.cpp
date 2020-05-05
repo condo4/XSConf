11f67e93c7993fd4694f77807af84f731d2acbb8
Fabien Proriol <fabien.proriol@viavisolutions.com>
2020-05-05 10:13:56 +0200

    Add first C++ implementation

---
+++ cxx/xsconf.cpp
@@ -0,0 +1,215 @@
+#include "xsconf.h"
+#include <fstream>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+
+static inline std::vector<std::string> split(const std::string& s, char seperator)
+{
+    std::vector<std::string> output;
+    std::string::size_type prev_pos = 0, pos = 0;
+    while((pos = s.find(seperator, pos)) != std::string::npos)
+    {
+        std::string substring( s.substr(prev_pos, pos-prev_pos) );
+        output.push_back(substring);
+        prev_pos = ++pos;
+    }
+    output.push_back(s.substr(prev_pos, pos-prev_pos)); // Last word
+    return output;
+}
+
+static inline std::string trim(const std::string& src, const std::string &remove = " \n\r\t")
+{
+    std::string str(src);
+    str.erase(0, str.find_first_not_of(remove));       //prefixing spaces
+    str.erase(str.find_last_not_of(remove)+1);         //surfixing spaces
+    return str;
+}
+
+static inline bool ends_with(std::string const & value, std::string const & ending)
+{
+    if (ending.size() > value.size()) return false;
+    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+}
+
+static inline std::vector<std::string> listdir(std::string dir)
+{
+    std::vector<std::string> files;
+    DIR *dp;
+    struct dirent *dirp;
+    dp  = opendir(dir.c_str());
+
+    if(dp)
+    {
+        while ((dirp = readdir(dp)) != nullptr)
+        {
+            files.push_back(std::string(dirp->d_name));
+        }
+        closedir(dp);
+    }
+
+    return files;
+}
+
+// Private Implementation
+class XSConf::XSConfPrivate
+{
+    std::string m_id;
+    std::map<std::string, std::string> m_map;
+    std::map<std::string, std::vector<std::string>> m_map_arrays;
+    std::string m_empty;
+    std::vector<std::string> m_empty_array;
+    mutable std::vector<std::string> m_keys;
+    mutable std::vector<std::string> m_keys_arrays;
+
+    int _parsePath(std::string path, bool dotd = false)
+    {
+        struct stat st;
+        int count = 0;
+        path += m_id + ".conf";
+        count += _parseFile(path);
+
+        if(dotd)
+        {
+            path += ".d";
+            if(stat(path.c_str(),&st) == 0 && ((st.st_mode & S_IFDIR) != 0))
+            {
+                for(auto& p: listdir(path))
+                {
+                    if(ends_with(path + "/" + p, ".conf"))
+                    {
+                        count += _parseFile(path + "/" + p);
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    int _parseFile(std::string file)
+    {
+        std::string line;
+        std::string url;
+        std::string topic; /* [General] is the root topic */
+        int count = 0;
+        bool add = false;
+        bool addarray = false;
+
+        if(access(file.c_str(), R_OK) != 0)
+        {
+            return 0;
+        }
+
+        std::ifstream infile(file);
+        while (std::getline(infile, line))
+        {
+            line = trim(line);
+            if(line[0] == '#')
+                continue;
+
+            if(line[0] == '[') /* Topic line */
+            {
+                topic = trim(line, "[]");
+                if(topic == "General")
+                    topic.clear();
+                continue;
+            }
+
+            std::vector<std::string> lineconf = split(line, '=');
+            if(lineconf.size() != 2)
+                continue;
+
+            std::string id = (topic.size())?(topic + "/" + lineconf[0]):(lineconf[0]);
+            if(lineconf[0][lineconf[0].size() - 1] == ']')
+            {
+                if(m_map_arrays.count(trim(id, "[]")) == 0)
+                {
+                    m_map_arrays[trim(id, "[]")] = {};
+                }
+                m_map_arrays[trim(id, "[]")].push_back(lineconf[1]);
+                addarray = true;
+                continue;
+            }
+
+            ++count;
+            add = true;
+            m_map[id] = lineconf[1];
+        }
+
+        if(add && m_keys.size())
+            m_keys.clear();
+        if(addarray && m_keys_arrays.size())
+            m_keys_arrays.clear();
+        return count;
+    }
+
+public:
+    XSConfPrivate(std::string id)
+        : m_id(id)
+    {
+        std::string home = std::getenv("HOME");
+
+        // Parse: /etc/<id>.conf and /etc/<id>.conf.d/*.conf
+        _parsePath("/etc/", true);
+
+        // Parse: ~/.config/<id>.conf
+        _parsePath(home + "/.config/");
+
+        // Parse: /var/xsconf/<id>.conf
+        _parsePath("/var/xsconf/");
+    }
+
+    const std::string &get(const XSConf &x, const std::string& id) const
+    {
+        if(m_map.count(id))
+        {
+            return m_map.at(id);
+        }
+        return m_empty;
+    }
+
+    const std::vector<std::string>& keys(const XSConf &x) const
+    {
+        if(m_keys.size() != m_map.size())
+        {
+            for(std::map<std::string,std::string>::const_iterator it = m_map.begin(); it != m_map.end(); ++it)
+            {
+                m_keys.push_back(it->first);
+            }
+        }
+        return m_keys;
+    }
+
+    const std::vector<std::string>& arrays(const XSConf &x) const
+    {
+        if(m_keys_arrays.size() != m_map_arrays.size())
+        {
+            for(std::map<std::string,std::vector<std::string>>::const_iterator it = m_map_arrays.begin(); it != m_map_arrays.end(); ++it)
+            {
+                m_keys_arrays.push_back(it->first);
+            }
+        }
+        return m_keys_arrays;
+    }
+
+    const std::vector<std::string>& array(const XSConf &x, const std::string &id) const
+    {
+        if(m_map_arrays.count(id))
+        {
+            return m_map_arrays.at(id);
+        }
+        return m_empty_array;
+    }
+
+};
+
+XSConf::XSConf(std::string id)
+    : pImpl{std::make_unique<XSConfPrivate>(id)}
+    {}
+
+const std::string &XSConf::operator[](const std::string &id) const { return pImpl->get(*this, id); }
+const std::vector<std::string>& XSConf::keys() const { return pImpl->keys(*this); }
+const std::vector<std::string>& XSConf::arrays() const { return pImpl->arrays(*this); }
+const std::vector<std::string>& XSConf::array(const std::string &id) const { return pImpl->array(*this, id); }
---
+++ cxx/xsconf.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <experimental/propagate_const>
+
+/*
+ * Source of configuration:
+ * RO /etc/<id>.conf
+ * RO /etc/<id>.conf.d/*.conf
+ * RO ~/.config/<id>.conf
+ * RW /var/xsconf/<id>.conf
+ */
+
+class XSConf
+{
+    class XSConfPrivate;
+    std::experimental::propagate_const< // const-forwarding pointer wrapper
+        std::unique_ptr<                // unique-ownership opaque pointer
+            XSConfPrivate>> pImpl;      // to the forward-declared implementation class
+
+public:
+    XSConf(std::string id);
+    const std::string &operator[](const std::string&) const;
+    const std::vector<std::string>& keys() const;
+    const std::vector<std::string>& arrays() const;
+    const std::vector<std::string>& array(const std::string &id) const;
+};
